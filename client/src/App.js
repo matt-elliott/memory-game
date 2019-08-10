10e039f550eb571bb5ae5cede1cf68572ea83367
Matt Elliott <mattelliottus@gmail.com>
2019-08-10 14:54:29 -0700

    added outline to gitignore, basic html layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container, Row, Col, Jumbotron} from 'react-bootstrap';
 
 function App() {
   return (
     <Container>
-      <Row className="bg-dark text-light text-center p-5">
+      <Row className="bg-dark text-light">
         <Col>
           <header>
             <h1>Memory Game</h1>
           </header>
         </Col>
+        <Col>
+          <div id="score">
+            <span id="score">Score: score</span>
+            <span id="top-score">Top Score: score</span>
+          </div>
+        </Col>
       </Row>
+      <Row>
+        <Jumbotron className="text-center">
+          <h2>
+            Click on an image to earn points, but don't click the same image twice - or you lose.
+          </h2>
+        </Jumbotron>
+      </Row>
+
     </Container>
   );
 }
